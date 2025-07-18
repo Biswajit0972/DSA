33440371c4d663e75b63ce576570057001d5dd90
Biswajit Das <workbiswajit0981@gmail.com>
2025-07-18 17:06:52 +0530

    Day 5: Add Linkedlist

---
+++ src/udemy/Day2/Solutions.java
@@ -0,0 +1,12 @@
+package udemy.Day2;
+
+public class Solutions {
+    public static void main(String[] args) {
+        int n1 = 23;
+        int n2 = n1;
+        n1 = 25;
+
+        System.out.println("n1" + n1);
+        System.out.println("n1" + n2);
+    }
+}
