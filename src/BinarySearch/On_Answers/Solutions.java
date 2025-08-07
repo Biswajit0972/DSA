ea5797192351d6eff26ad2b5b30cec60df28388b
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-07 20:25:16 +0530

    Day 15 on leet code

---
+++ src/BinarySearch/On_Answers/Solutions.java
@@ -0,0 +1,11 @@
+package BinarySearch.On_Answers;
+
+public class Solutions {
+
+    /*
+    * Binary search on Answer is an algorithm, where we're finding answer based on particular conditions. Basically, Questions gives search space in which we take mid and searching best answer.
+    * */
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(3/4));
+    }
+}
