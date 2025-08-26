f6970b63b15657263748ed46487f8343c07592ee
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-26 11:53:56 +0530

    26 AUG LinkedList

--- src/LinkedList_Aug22/Solutions.java
+++ src/LinkedList_Aug22/Solutions.java
@@ -50,6 +50,38 @@ public class Solutions {
         head.next = null;
         return head;
     }
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        boolean hasCycle = false;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (!hasCycle) {
+            return null;
+        }
+
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow ;
+    }
 }
 
