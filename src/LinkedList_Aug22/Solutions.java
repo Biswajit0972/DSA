c5878ab78c63ef11255e8cfbea582bbd50b7ac1d
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-27 16:37:59 +0530

    27 AUG LinkedList

--- src/LinkedList_Aug22/Solutions.java
+++ src/LinkedList_Aug22/Solutions.java
@@ -187,6 +187,29 @@ public class Solutions {
         return count;
     }
 
+    public ListNode removeNthFromEndOnePass(ListNode head, int n) {
+        ListNode  dummy = new ListNode(-1);
+        dummy.next = head;
 
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            slow  = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
 }
 
