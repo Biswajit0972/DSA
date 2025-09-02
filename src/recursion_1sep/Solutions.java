cf14c050bec3efe94af04d63d788803089cc450c
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-02 07:09:21 +0530

    2 sep

--- src/recursion_1sep/Solutions.java
+++ src/recursion_1sep/Solutions.java
@@ -20,4 +20,24 @@ public class Solutions {
             return binaryExpoentiation(x, y / 2);
         }
     }
+    int M = (int)1e9 + 7;
+
+    public int countGoodNumbers(long n) {
+        long evenPosition = (n + 1) / 2; // digits at even indices → choices = 5
+        long oddPosition = n / 2;        // digits at odd indices → choices = 4
+
+        long result = (pow(5, evenPosition) * pow(4, oddPosition)) % M;
+        return (int) result;
+    }
+
+
+    public long pow(long base, long power) {
+        if (power == 0) return 1;
+        long half = pow(base, power / 2);
+        long res = (half * half) % M;
+        if (power % 2 == 1) {
+            res = (res * base) % M;
+        }
+        return res;
+    }
 }
