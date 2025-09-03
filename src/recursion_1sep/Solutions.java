1f5a2a75a9770bdee5286127a3e2107decf96d83
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-03 09:38:49 +0530

    3 sep

--- src/recursion_1sep/Solutions.java
+++ src/recursion_1sep/Solutions.java
@@ -158,4 +158,31 @@ public class Solutions {
             sb.deleteCharAt(sb.length() - 1);
         }
     }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        generateParenthesis(n, 0, 0, sb, res);
+        return res;
+    }
+
+    public void generateParenthesis(int n, int open, int close, StringBuilder sb, List<String> res) {
+
+        if (open == n && close == n) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < n) {
+            sb.append('(');
+            generateParenthesis(n, open + 1, close, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (close < open) {
+            sb.append(')');
+            generateParenthesis(n, open, close + 1, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
 }
