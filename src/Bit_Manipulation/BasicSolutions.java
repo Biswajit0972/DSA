bacfd29b900e7a5850ca2b6195cabee024bf6d0b
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-05 15:21:18 +0530

    5 sep  Bit manipulation

--- src/Bit_Manipulation/BasicSolutions.java
+++ src/Bit_Manipulation/BasicSolutions.java
@@ -2,7 +2,8 @@ package Bit_Manipulation;
 
 public class BasicSolutions {
     public static void main(String[] args) {
-        System.out.println(intToBinary(13));
+        System.out.println(binaryToInt(intToBinary(13)));
+
     }
 
     public static String intToBinary(int num) {
@@ -20,5 +21,18 @@ public class BasicSolutions {
             num /= 2;
         }
         return sb.toString();
+    }
+
+    public static int binaryToInt(String s) {
+        int res = 0;
+        int power = 1;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == '1') {
+                res += power;
+            }
+            power *= 2;
+        }
+
+        return res;
     }
 }
