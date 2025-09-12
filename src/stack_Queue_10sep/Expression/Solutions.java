7a64ddbd837c71e9b5d58a951980ff19761ecab0
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-12 10:21:13 +0530

    Add infixToPostfix conversion method to Solutions class

---
+++ src/stack_Queue_10sep/Expression/Solutions.java
@@ -0,0 +1,64 @@
+package stack_Queue_10sep.Expression;
+
+import java.util.Stack;
+
+public class Solutions {
+    public static void main(String[] args) {
+        String exp = "h^m^q^(7-4)";
+        System.out.println(infixToPostfix(exp));
+    }
+
+    public static String infixToPostfix(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for  (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (isOperand(ch)) {
+                sb.append(ch);
+            }else if (ch == '(') {
+                st.push(ch);
+            }else if (ch == ')') {
+                while (!st.isEmpty() && st.peek() != '(') {
+                    sb.append(st.pop());
+                }
+                st.pop();
+            }else if (ch == '^') {
+                st.push(ch);
+            }
+            else {
+                while (!st.isEmpty() && relativePriority(ch) <= relativePriority(st.peek())) {
+                    sb.append(st.pop());
+                }
+                st.push(ch);
+            }
+        }
+
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        return sb.toString();
+    }
+    public static String postToInfix(String exp) {
+
+    }
+
+
+    public static int relativePriority (char ch) {
+        if (ch == '+' || ch == '-') {
+            return 1;
+        }else if (ch == '*' || ch == '/') {
+            return 2;
+        }else if (ch == '^') {
+            return 3;
+        }else {
+            return -1;
+        }
+    }
+
+    public static boolean  isOperand (char ch) {
+        return ch >= '0' && ch <= '9' || ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z';
+    }
+}
