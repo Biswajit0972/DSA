9f0a6503116ed39ae0c8079af9b58e7dc41e876b
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-15 07:59:02 +0530

    previous commit  13 sep

--- src/stack_Queue_10sep/Expression/Solutions.java
+++ src/stack_Queue_10sep/Expression/Solutions.java
@@ -4,8 +4,8 @@ import java.util.Stack;
 
 public class Solutions {
     public static void main(String[] args) {
-        String exp = "h^m^q^(7-4)";
-        System.out.println(infixToPostfix(exp));
+        String exp = "(a-b/c)*(a/k-l)";
+        System.out.println(infixToPrefix(exp));
     }
 
     public static String infixToPostfix(String s) {
@@ -41,6 +41,46 @@ public class Solutions {
 
         return sb.toString();
     }
+    public static String infixToPostfix2(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for  (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (isOperand(ch)) {
+                sb.append(ch);
+            }else if (ch == '(') {
+                st.push(ch);
+            }else if (ch == ')') {
+                while (!st.isEmpty() && st.peek() != '(') {
+                    sb.append(st.pop());
+                }
+                st.pop();
+            }
+            else {
+                if (ch == '^') {
+                    while (!st.isEmpty()  && st.peek() == '^') {
+                        sb.append(st.pop());
+                    }
+                    st.push(ch);
+
+                }else {
+                    while (!st.isEmpty() && relativePriority(ch) <= relativePriority(st.peek())) {
+                        sb.append(st.pop());
+                    }
+                    st.push(ch);
+                }
+
+            }
+        }
+
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        return sb.toString();
+    }
 
     public static String postToInfix(String exp) {
         if (exp == null || exp.isEmpty()) {
@@ -63,6 +103,43 @@ public class Solutions {
         return st.pop();
     }
 
+    public static String infixToPrefix(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        s = reverse(s);
+
+        String postFix  = infixToPostfix2(s);
+
+        return reverse(postFix);
+    }
+
+    public static String  reverse (String s) {
+        int n = s.length();
+
+        if (n == 1) {
+            return s;
+        }
+
+        char[] arr = s.toCharArray();
+
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            if (temp == '(' && arr[right] == ')') {
+                left++;
+                right--;
+                continue;
+            }
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
 
     public static int relativePriority (char ch) {
         if (ch == '+' || ch == '-') {
