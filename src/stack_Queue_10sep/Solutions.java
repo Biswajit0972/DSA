056270341924ee78cf2144bd5d2fe9dfc7b87332
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-17 16:46:35 +0530

    Add sumSubarrayMins method to Solutions class for calculating sum of minimums of all subarrays

--- src/stack_Queue_10sep/Solutions.java
+++ src/stack_Queue_10sep/Solutions.java
@@ -145,8 +145,22 @@ public class Solutions {
         return trapWater;
     }
 
+    public static int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        for  (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
+                min = Math.min(min, arr[j]);
+                sum += min;
+            }
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
-      int [] nums = {4,2,0,3,2,5};
-        System.out.println(trap(nums));
+      int [] nums = {3,1,2,4};
+        System.out.println(sumSubarrayMins(nums));
 
 }}
