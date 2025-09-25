c431f0542d61e5242808355d888aa7fcb649f242
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-25 20:15:43 +0530

    Optimize longestSubarray method to use HashMap for improved performance in finding the longest subarray with sum equal to k

--- src/Revision/Week1/Solutions.java
+++ src/Revision/Week1/Solutions.java
@@ -1,11 +1,13 @@
 package Revision.Week1;
 
+import java.util.HashMap;
+
 public class Solutions {
 
     // ! Revision of Week 1
     //? Question 1: Longest SubArray Sum EqualTo k
     //* Brute Force
-
+    /*
     public static int longestSubarray(int[] arr, int k) {
         int maxLength = 0;
 
@@ -22,9 +24,34 @@ public class Solutions {
 
         return maxLength;
     }
+    */
+
+    // ! Question 1: Longest SubArray Sum EqualTo k - Optimized
+
+    public static int longestSubarray(int[] arr, int k) {
+        int maxLength = 0;
+        HashMap<Integer, Integer> hash =  new HashMap<>();
+        int preSum  = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            preSum += arr[i];
+
+            if (preSum == k) {
+                maxLength =  Math.max(maxLength, i + 1);
+            }else if (hash.containsKey(preSum - k)) {
+                maxLength = Math.max(maxLength, i - hash.get(preSum - k));
+            }
+
+           if (!hash.containsKey(preSum)) {
+               hash.put(preSum, i);
+           }
+        }
+
+        return maxLength;
+    }
 
     public static void main(String[] args) {
-        int [] testingArr = {-5, 8, -14, 2, 4, 12};
+        int[] testingArr = {-5, 8, -14, 2, 4, 12};
         System.out.println(longestSubarray(testingArr, -5));
     }
 }
