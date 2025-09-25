6af5770d957b274fa4c2a75f76b9542f2aee86ea
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-25 18:42:00 +0530

    Add longestSubarray method to Solutions class for finding the longest subarray with sum equal to k

--- src/Revision/Week1/Solutions.java
+++ src/Revision/Week1/Solutions.java
@@ -1,7 +1,30 @@
 package Revision.Week1;
 
 public class Solutions {
-    public static void main(String[] args) {
 
+    // ! Revision of Week 1
+    //? Question 1: Longest SubArray Sum EqualTo k
+    //* Brute Force
+
+    public static int longestSubarray(int[] arr, int k) {
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+                int sum = 0;
+            for (int j = i ; j < arr.length ; j++) {
+                sum += arr[j];
+
+                if (sum == k) {
+                    maxLength =  Math.max(maxLength, j - i +1);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int [] testingArr = {-5, 8, -14, 2, 4, 12};
+        System.out.println(longestSubarray(testingArr, -5));
     }
 }
