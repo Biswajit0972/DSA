dd840ba7ea26f494121697b1e3243ed79c58b611
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-29 11:48:58 +0530

    Add findMaxAverage method to calculate the maximum average of a subarray of size k using sliding window technique

--- src/sliding_window_27sep/Solutions.java
+++ src/sliding_window_27sep/Solutions.java
@@ -105,6 +105,25 @@ public class Solutions {
         return length;
     }
 
+    public double findMaxAverage(int[] nums, int k) {
+        double maxAvg = 0.0;
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        maxAvg = sum / k;
+
+        for (int i = k; i < nums.length; i++) {
+            sum -= nums[i - k];
+            sum += nums[i];
+
+            maxAvg = Math.max(maxAvg, (sum/ k));
+        }
+
+        return maxAvg;
+    }
+
     public static void main(String[] args) {
         System.out.println(lengthOfLongestSubstringOptimal("pwwkew"));
     }
