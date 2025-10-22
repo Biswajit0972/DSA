d5e77b0a00b584ecd8d0329a0399abe3cc602c3c
Biswajit Das <workbiswajit0981@gmail.com>
2025-10-22 09:18:44 +0530

    Add threeSum method to find all unique triplets in an array that sum to zero using sorted two-pointer approach

--- src/GameOn60Day/Phase_I/Week_1/Day1/Solutions.java
+++ src/GameOn60Day/Phase_I/Week_1/Day1/Solutions.java
@@ -1,7 +1,6 @@
 package GameOn60Day.Phase_I.Week_1.Day1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solutions {
     /**
@@ -66,7 +65,56 @@ public class Solutions {
     
     return res;
 }
-    
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+//        HashSet<List<Integer>> set = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i+1; j < nums.length; j++) {
+//                for (int k = j+1; k < nums.length; k++) {
+//                    if (nums[i] + nums[j] + nums[k] == 0) {
+//                        List<Integer> curr = new ArrayList<>();
+//                        curr.add(nums[i]);
+//                        curr.add(nums[j]);
+//                        curr.add(nums[k]);
+//                        Collections.sort(curr);
+//                        set.add(curr);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        res.addAll(set);
+//        return res;
+        int i =0;
+        int left = i + 1;
+        int right = nums.length - 1;
+        Arrays.sort(nums);
+
+        while (left < right ) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0) {
+                List<Integer> curr = new ArrayList<>(3);
+                curr.add(nums[i]);
+                curr.add(nums[left]);
+                curr.add(nums[right]);
+                res.add(curr);
+                i++;
+                left++;
+                right--;
+            }else if (sum  > 0) {
+                right--;
+            }else {
+                i++;
+                left++;
+            }
+        }
+        return res;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println(majorityElement(new int[]{1,1,1,3,3,2,2,2}));
     }
