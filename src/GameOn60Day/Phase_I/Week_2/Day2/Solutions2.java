6f867108b19f271b89c4906388a6baf38c75bd9c
Biswajit Das <workbiswajit0981@gmail.com>
2025-10-30 08:36:38 +0530

    Refactor copyRandomList method to optimize linked list cloning by restructuring logic; add printList method and example test case for better validation

--- src/GameOn60Day/Phase_I/Week_2/Day2/Solutions2.java
+++ src/GameOn60Day/Phase_I/Week_2/Day2/Solutions2.java
@@ -17,25 +17,69 @@ public class Solutions2 {
         public Node copyRandomList(Node head) {
             if (head == null) return null;
 
-            HashMap<Node, Node> map = new HashMap<>();
+            //! copy the node
             Node temp = head;
+
+            while (temp  != null ) {
+                Node newNode =  new Node(temp.val);
+                newNode.next = temp.next;
+                temp.next = newNode;
+                temp = newNode.next;
+            }
+
+            //! random pointer fixing
+            temp = head;
+
             while (temp != null) {
-                map.put(temp, new Node(temp.val));
+                if (temp.random != null) {
+                    temp.next.random = temp.random.next;
+                }
+                temp = temp.next.next;
+            }
+
+            // * create a separated list
+
+            temp = head;
+            Node copyHead = temp.next;
+            Node copy = copyHead;
+
+            while (temp != null) {
+                temp.next = copy.next;
+                temp = temp.next;
+                if (temp != null)  {
+                    copy.next = temp.next;
+                    copy = temp.next;
+                }
+            }
+            return copyHead;
+        }
+
+        public void printList(Node head) {
+            if (head == null) {
+                return;
+            }
+
+            Node temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.val + "->");
                 temp = temp.next;
             }
-
-            Node dummy = map.get(head);
-
-            for (Node node : map.keySet()) {
-                map.get(node).next = map.get(node.next);
-                map.get(node).random = map.get(node.random);
-            }
-
-            return dummy;
+            System.out.println("Null");
         }
     }
 
     public static void main(String[] args) {
-
+        Node e1 = new Node(1);
+        Node e2 = new Node(2);
+        Node e3 = new Node(3);
+        Node e4 = new Node(4);
+        Node e5 = new Node(5);
+        e1.next = e2;
+        e2.next = e3;
+        e3.next = e4;
+        e4.next = e5;
+        e1.copyRandomList(e1);
+        e1.printList(e1);
     }
 }
