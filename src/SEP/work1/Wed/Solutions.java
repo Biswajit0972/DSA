6cfa24aa79502574fcd23531b28eb2d28484e2b1
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-05 08:35:48 +0530

    Add fractionalKnapsack method to Solutions.java to compute maximum value for a knapsack with fractional items using a greedy approach; include test case in main method

--- src/SEP/work1/Wed/Solutions.java
+++ src/SEP/work1/Wed/Solutions.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Solutions {
     /*
-    * 455. Assign Cookies (GREEDY)
-    * */
+     * 455. Assign Cookies (GREEDY)
+     * */
     public static int findContentChildren(int[] g, int[] s) {
         //! count for satisfied children
         int satisfied = 0;
@@ -17,7 +17,7 @@ public class Solutions {
         int i = 0;
         int j = 0;
 
-        while (i < g.length &&   j  < s.length) {
+        while (i < g.length && j < s.length) {
             if (g[i] <= s[j]) {
                 satisfied++;
                 i++;
@@ -27,7 +27,50 @@ public class Solutions {
         return satisfied;
     }
 
-    public static void main(String[] args) {
+    static class Item {
+        int val;
+        int wt;
+        double ratio;
 
+        public Item(int val, int wt) {
+            this.val = val;
+            this.wt = wt;
+            this.ratio = (double) val / wt;
+        }
+    }
+
+        public static double fractionalKnapsack(int[] val, int[] wt, int capacity) {
+      Item [] items = new Item[val.length];
+
+      for (int i = 0; i < val.length; i++) {
+          items[i] = new Item(val[i], wt[i]);
+      }
+
+      Arrays.sort(items, (a,b) -> Double.compare(b.ratio,  a.ratio));
+
+      for (Item item : items) {
+          System.out.println(item.ratio);
+      }
+      double total = 0;
+
+      for (int i =0; i < items.length; i++) {
+
+          if (items[i].wt <= capacity) {
+              total += items[i].val;
+              capacity -= items[i].wt;
+          }else {
+
+             double fracPrice = items[i].val * ((double) capacity / items[i].wt);
+             total += fracPrice;
+             break;
+          }
+      }
+
+      return total;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fractionalKnapsack(new int[]{60, 100, 120}, new int[]{10, 20, 30}, 50));
     }
 }
