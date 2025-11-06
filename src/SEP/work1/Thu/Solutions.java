cf385780cdf00e4b12af40156f646af07229f784
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-06 09:35:20 +0530

    Add maxMeetings method to Solutions.java to compute maximum number of non-overlapping meetings using a greedy approach; update main method with test case

--- src/SEP/work1/Thu/Solutions.java
+++ src/SEP/work1/Thu/Solutions.java
@@ -1,5 +1,7 @@
 package SEP.work1.Thu;
 
+import java.util.Arrays;
+
 public class Solutions {
     public static boolean checkValidString(String s) {
        int min =0;
@@ -50,7 +52,43 @@ public class Solutions {
 
     }
 
+    static class  Meeting {
+        int start;
+        int end;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+
+    public static int maxMeetings(int start[], int end[]) {
+       Meeting [] meetings = new Meeting[start.length];
+       for (int i = 0; i < start.length; i++) {
+           meetings[i] = new Meeting(start[i], end[i]);
+       }
+
+       //! sort the meetings by thier ending time
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a.end, b.end));
+
+       for (int i = 0; i < meetings.length; i++) {
+           System.out.println(meetings[i].start + " " + meetings[i].end);
+       }
+       int count = 1;
+       Meeting curr = meetings[0];
+
+       for (int i = 1; i < meetings.length; i++) {
+          if (curr.end  < meetings[i].start) {
+              count++;
+              curr = meetings[i];
+          }
+       }
+
+       return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(checkValidString("**************************************************))))))))))))))))))))))))))))))))))))))))))))))))))))"));
+        System.out.println(maxMeetings(new int[]{1, 3, 0, 5, 8, 5}, new int[]{2, 4, 6, 7, 9, 9}));
     }
 }
